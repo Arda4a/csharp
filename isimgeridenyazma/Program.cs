df14fc2fcc89d4d10c0f5ebf7ea346b83d814f4f
Arda4a <119483512+Arda4a@users.noreply.github.com>
2022-12-28 16:55:29 +0300

    Add files via upload

---
+++ isimgeridenyazma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace isimgeridenyazma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
