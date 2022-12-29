b7da3d92388b3e5d9553a753c4ef42282c184646
Arda4a <119483512+Arda4a@users.noreply.github.com>
2022-12-29 23:30:57 +0300

    Add files via upload

---
+++ isimgeridenyazma/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace isimgeridenyazma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         string isim="arda";
+string tersisim = "";
+            foreach (char harf in isim)
+            {
+               tersisim = harf.ToString() + tersisim;
+            }
+            Console.WriteLine(tersisim);
+        }
+    }
+}
